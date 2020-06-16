923e297c444e2d6173ab15321cfbbd2411eef868
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-16 16:30:15 +0200

    not babel

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
-import express from 'express';
+var express = require('express');
+var app = express();
 
-const PORT = process.env.HTTP_PORT || 4001;
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('just gonna send it')
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.get('/flower', (req, res) => {
-    res.json({
-        name: 'Dandelion',
-        colour: 'Blue-ish'
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}.`);
-});
+app.listen(8000);
