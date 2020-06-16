935f53e16493b7c8d5c656391cd0078757df1db9
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-16 18:26:49 +0200

    check rolling

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Check Rolling!');
+  res.send('Check Rolling Now?');
 });
 
 app.listen(3000, () => console.log(`Listening on port 3000.`));
