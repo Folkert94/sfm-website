fa5d167f9c5ba54fee3ff3299269e0b1b14b56fd
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-16 18:28:30 +0200

    added req

--- src/server.js
+++ src/server.js
@@ -6,4 +6,8 @@ app.get('/', function (req, res) {
   res.send('Check Rolling Now?');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('who dis?');
+});
+
 app.listen(3000, () => console.log(`Listening on port 3000.`));
