f3ae39d351c26bb314894fe54034a8e9dc456109
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-16 16:39:34 +0200

    port 3000

--- src/server.js
+++ src/server.js
@@ -7,4 +7,4 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+app.listen(3000, () => console.log(`Listening on port 3000.`));
