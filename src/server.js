c283bf481a7e4e7c40b371d43fbe87fabd2ad634
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-17 16:37:37 +0200

    website skeleton

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Check Rolling Now?');
-});
+app.use(express.static(path.join(__dirname, '/build')))
 
-app.get('/hello', (req, res) => {
-    res.send('who dis?');
-});
+// app.get('/', (req, res) => {
+//   res.send('Check Rolling Now?');
+// });
+//
+// app.get('/hello', (req, res) => {
+//     res.send('who dis?');
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
 
 app.listen(3000, () => console.log(`Listening on port 3000.`));
