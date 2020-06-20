94a27e558e51d228ab56aad0df152324ad03f3bf
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-20 23:04:06 +0200

    navigation toggle & font

--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8ccd87861cffaf3640096f1d885cfe9.js"
+  "/precache-manifest.15fceab777107b0136d095daba1819fe.js"
 );
 
 self.addEventListener('message', (event) => {
