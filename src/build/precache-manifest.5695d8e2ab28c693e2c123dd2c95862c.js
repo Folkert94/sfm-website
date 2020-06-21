e94eac9c523b6a26bd241a18f395b4fcd9b336ab
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-21 13:30:55 +0200

    adjustment blog & css services

---
+++ src/build/precache-manifest.5695d8e2ab28c693e2c123dd2c95862c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e92492a1a5ab232c5a4beb19718dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b5566e8b2d5508e37e",
+    "url": "/static/css/2.f030d906.chunk.css"
+  },
+  {
+    "revision": "79a94f7b5d561b67d89d",
+    "url": "/static/css/main.b61950a2.chunk.css"
+  },
+  {
+    "revision": "c9b5566e8b2d5508e37e",
+    "url": "/static/js/2.2eeb9a9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2eeb9a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a94f7b5d561b67d89d",
+    "url": "/static/js/main.edd8902a.chunk.js"
+  },
+  {
+    "revision": "e4929f05d22c9f5546c1",
+    "url": "/static/js/runtime-main.19efa801.js"
+  },
+  {
+    "revision": "022af2f62a7c0af68de51fa2cbf218e2",
+    "url": "/static/media/iCiel Gotham-Medium.022af2f6.ttf"
+  },
+  {
+    "revision": "bb9b4311304cd16e235a5b5607debcf3",
+    "url": "/static/media/sfm-logo.bb9b4311.svg"
+  }
+]);
--- src/build/service-worker.js
+++ src/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c83f88677f47c38f013e5fc39c455a5.js"
+  "/precache-manifest.5695d8e2ab28c693e2c123dd2c95862c.js"
 );
 
 self.addEventListener('message', (event) => {
