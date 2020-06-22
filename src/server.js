6546999296284e7c94d5c8271bce08383061b9f5
Folkert Stijnman <folkertstijnman@hotmail.com>
2020-06-22 17:28:26 +0200

    api init

--- src/server.js
+++ src/server.js
@@ -1,17 +1,50 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 import path from 'path';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/build')))
+app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   res.send('Check Rolling Now?');
-// });
-//
-// app.get('/hello', (req, res) => {
-//     res.send('who dis?');
-// });
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+        const db = client.db('sfm-blog');
+
+        await operations(db);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
+
+app.get('/api/blog-post/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+
+        const articleInfo = await db.collection('blogPosts').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    }, res);
+})
+
+app.post('/api/blog-post/:name/addpost', (req, res) => {
+    const newArticle = req.body;
+    const articleName = req.params.name;
+
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('blogPosts').insert(newArticle)
+
+        res.status(200).json(newArticle);
+    }, res);
+
+});
+
+// edit post
+// delete post
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
